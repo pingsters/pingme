1698ef25680375280ef2d8abdec5fcbcc5188196
Youngchan Je <ochanje210@gmail.com>
2016-12-13 23:36:41 +0900

    update server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+app.get('/rotate/:value', function (req, res) {
+  socket.emit('rotate', {angle: req.params.value});
+  res.end("bitch" + req.params.value);
+});
+
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
